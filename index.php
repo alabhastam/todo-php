0eae2e623e3c0d01386489a9ee7903ca62f2d080
alitorpedo <abdollahia713@gmail.com>
2024-07-24 15:08:39 +0330

    ready to go

--- index.php
+++ index.php
@@ -1,17 +1,32 @@
 <?php
+include "bootstrap/init.php";
 
-define('ROOT_URL', 'C:/xampp/htdocs/todo-php/');
+if(isset($_GET['logout'])){
+    logout();
+}
 
-include "init.php"; 
+if(!isLoggedIn()){
+    // redirect to aut form
+    header("Location: " . site_url('auth.php'));
+}
+# user is LoggedIn
+$user = getLoggedInUser();
 
+if(isset($_GET['delete_folder']) && is_numeric($_GET['delete_folder'])){
+    $deletedCount = deleteFolder($_GET['delete_folder']);
+    // echo "$deletedCount folders succesfully deleted!";
+}
 
-if (isset($_GET['delete_folder']) && is_numeric($_GET['delete_folder'])) {
-    deleteFolder($_GET['delete_folder']);
+if(isset($_GET['delete_task']) && is_numeric($_GET['delete_task'])){
+    $deletedCount = deleteTask($_GET['delete_task']);
+    // echo "$deletedCount Tasks succesfully deleted!";
 }
 
 
+# connect to db and get tasks
 $folders = getFolders();
 
-include ROOT_URL."tls/tls-index.php";
 
- ?>
+$tasks = getTasks();
+
+include "tpl/tpl-index.php";
--- process/ajaxHand.php
+++ process/ajaxHand.php
@@ -1,5 +1,5 @@
 <?php
-include_once "./init.php";
+include "../init.php";
 addFolder($_POST['folderName']);
 ?>
 
